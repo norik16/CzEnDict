1d51908aa313d24d2d1476399d7bf2fc0af00d4b
Martin Miksik <mamiksik@gmail.com>
2017-06-29 18:50:43 +0200

    Initial Commit

---
+++ Slovnik-CZ-EN/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Slovnik-CZ-EN
+//
+//  Created by Martin Miksik on 29/06/2017.
+//  Copyright © 2017 Martin Miksik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
